8d682367077b7e0469ed85701e8aa73fb5656734
Mariya <va.mariya@yahoo.com>
2021-11-06 22:18:29 +0000

    Pushing word-search pair-programming

--- wordsearch.js
+++ wordsearch.js
@@ -2,7 +2,10 @@ const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
-    }
+    } return false
 }
 
 module.exports = wordSearch
+
+
+
